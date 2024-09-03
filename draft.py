b7787730cf61a83cfcef4f49faf830444628db6c
antony1140 <67793865+antony1140@users.noreply.github.com>
2024-09-02 21:34:34 -0400

    problem set 1.3

--- draft.py
+++ draft.py
@@ -1,18 +1,33 @@
+def func(s):
+    sub = ''
+    longest = ''
+    for i in range(len(s)-1):
+        if s[i] <= s[i+1]:
+            sub += s[i+1]
+            i += 2
+        else:
+            sub = ''
+        if len(sub) > len(longest):
+            longest = sub
+        print(sub)
+    print(longest)
+
+
+# final working code
+def func2(s):
+    sub = s[0]
+    longest = ''
+    for i in range(len(s)-1):
+        if s[i] <= s[i+1]:
+            sub += s[i+1]
+            i += 2
+        else:
+            sub = s[i+1]
+        if len(sub) > len(longest):
+            longest = sub
+        print(sub)
+    print('Longest substring in alphabetical order is: ' + longest)
+
 
 s = 'azcbobobegghakl'
-count = 0
-for x in s:
-    if (x == 'a' or x == 'o' or x == 'e' or x == 'i' or x == 'u'):
-        count = count + 1
-
-print('Number of vowels: ' + str(count))
-
-
-count = 0
-spot = 0
-for x in s:
-    spot = spot + 1
-    if x == 'b':
-        if s[spot] == 'o' and s[spot+1] == 'b':
-            count = count + 1
-print('number of times bob occurs: ' + str(count))
+func2(s)
