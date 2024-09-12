d2198e0cb7daf133a684638f718f066466f3a6ec
antony1140 <67793865+antony1140@users.noreply.github.com>
2024-09-12 01:20:53 -0400

    ps2-1 and 2-2

--- draft.py
+++ draft.py
@@ -1,17 +1,38 @@
-# final working code
-def func(s):
-    sub = s[0]
-    longest = ''
-    for i in range(len(s)-1):
-        if s[i] <= s[i+1]:
-            sub += s[i+1]
-        else:
-            sub = s[i+1]
-        if len(sub) > len(longest):
-            longest = sub
-        print(sub)
-    print('Longest substring in alphabetical order is: ' + longest)
+from time import perf_counter
 
 
-s = 'azcbobobegghakl'
-func(s)
+# 2.1
+def func(initialBalance, interestRate, paymentRate, termLength):
+    unpaid = initialBalance
+    monthlyInt = interestRate/12
+    for i in range(termLength):
+        paidBalance = unpaid - (unpaid*paymentRate)
+        unpaid = paidBalance + (paidBalance * monthlyInt)
+    print("remaining balance: %.2f" % unpaid)
+
+
+func(5000, .18, .02, 12)
+
+
+# 2.2
+def func2(initialBalance, interestRate, termLength):
+    balance = initialBalance
+    payment = 0
+    monthlyInt = interestRate/12
+    while (balance > 0):
+        balance = initialBalance
+        payment += 10
+        for i in range(termLength):
+            paidBalance = balance - payment
+            balance = paidBalance + (paidBalance * monthlyInt)
+    print('Lowest payment: ' + str(payment))
+
+
+amount = input('enter a starting balance: ')
+start = perf_counter()
+func2(int(amount), .18, 12)
+end = perf_counter()
+print(end - start)
+
+
+# 2.4
